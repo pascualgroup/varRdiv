e0f4cfc3e5196342e6b616cdf8ea4a0eed900ccf
Qixin He <heqixin@gmail.com>
2020-06-03 19:33:32 -0500

    var model for Rdiv networks exploration

---
+++ src/datamodel/Gene.hpp
@@ -0,0 +1,35 @@
+#ifndef Gene_hpp
+#define Gene_hpp
+
+#include <array>
+#include "parameters.hpp"
+
+namespace varmodel {
+
+enum GeneSource {
+    SOURCE_POOL = 0,
+    SOURCE_RECOMBINATION = 1,
+    SOURCE_MUTATION = 2,
+    SOURCE_IMMIGRATION = 3
+};
+
+struct Gene {
+    Gene(uint64_t id) : id(id) { }
+    
+    uint64_t const id;
+    
+    uint64_t source;
+    double is_functional;
+    uint64_t upsGroup;
+    std::array<uint64_t, N_LOCI> alleles;
+    double dur;
+    double avTransRate;
+    double originTime;
+    double deathTime;
+    double recombRate;
+    uint64_t refcount;
+};
+
+} // namespace varmodel
+
+#endif // #ifndef Gene_hpp
